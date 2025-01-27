d25f0a91075badf5b41b11590d1a242ab173183b
Aaditya <aaditya9feb@gmail.com>
2025-01-27 12:20:35 +0530

    factorial function

--- Python_workshop/day7.py
+++ Python_workshop/day7.py
@@ -12,23 +12,49 @@
 
 ######################################################################################################
 
-def findEvenNumbers(list):
-    even_numbers = []
-    for i in list:
-        if i % 2 == 0:
-            even_numbers.append(i)
-    return even_numbers
+# def findEvenNumbers(list):
+#     even_numbers = []
+#     for i in list:
+#         if i % 2 == 0:
+#             even_numbers.append(i)
+#     return even_numbers
 
-list=[1,2,3,4,5,6,7,8,9,10]
-print(findEvenNumbers(list))
+# list=[1,2,3,4,5,6,7,8,9,10]
+# print(findEvenNumbers(list))
 
 ######################################################################################################
 
-def name(name):
-    def stars():
-        print("************")
-    stars()
-    print(name)
-    stars()
+# def name(name):
+#     def stars():
+#         print("************")
+#     stars()
+#     print(name)
+#     stars()
     
-name("Aaditya")
+# name("Aaditya")
+
+######################################################################################################
+
+def my_decorator(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper
+
+@my_decorator
+def say_hello():
+    print("Hello!")
+
+say_hello()
+
+######################################################################################################
+
+# write a function to find factorial of a number#
+
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+
+print(factorial(5))
